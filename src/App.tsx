6b65e3e88af297762e91da3e0bc62c5388f3107f
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-02-08 00:36:54 -0300

    feat(product): adicionado modal de transação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewModalOpen, setIsNewModalOpen] = useState(false);
+  function onHandleOpenNewTransaction() {
+    setIsNewModalOpen(true);
+  }
+
+  function onHandleCloseNewTransaction() {
+    setIsNewModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onHandleOpenNewTransaction={onHandleOpenNewTransaction} />
       <Dashboard />
+      <Modal
+        isOpen={isNewModalOpen}
+        onRequestClose={onHandleCloseNewTransaction}
+      >
+        <h1>Cadastra transação</h1>
+      </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onHandleOpenNewTransaction: () => void;
+}
+
+export function Header({ onHandleOpenNewTransaction }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logo} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onHandleOpenNewTransaction}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
